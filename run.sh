247ef4c74051c9a4fd0c5482810115b0adab1ff2
Borys Kopeć <boryskopec@proton.me>
2026-01-31 19:34:13 +0100

    Basic setup of the program

---
+++ run.sh
@@ -0,0 +1,1 @@
+cargo run --release -- body poem.txt
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() < 3 {
+        panic!("Too few arguments. Expected PATTERN and at least one FILE.")
+    }
+
+    let pattern = &args[1];
+    let files = &args[2..];
+
+    files.iter().for_each(|file: &String| {
+        search(file, pattern);
+    });
+}
+
+fn search(file: &String, pattern: &String) {
+    match std::fs::read_to_string(file) {
+        Err(e) => eprintln!("Can't read contents of {file}: {e}"),
+        Ok(contents) => println!("{pattern}\n{contents}"),
+    }
 }
